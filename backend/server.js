1a6887e583f39289afbb58391bd5c4e76eb84c17
Jaideep Bose <jaideepbose51@gmail.com>
2025-02-06 19:20:53 +0530

    added admin funtionality

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,8 @@ import cors from "cors";
 import dotenv from "dotenv";
 dotenv.config();
 import connectDB from "./config/db.js";
-// import connectCloudinary from './config/cloudinary.js'
-// import adminRouter from './routes/adminRoute.js'  
+import connectCloudinary from './config/cloudinary.js'
+import adminRouter from './routes/adminRoute.js'  
 import doctorRouter from './routes/doctorRoute.js'
 import userRouter from './routes/userRoute.js'
 import chatBot from './routes/chatRoute.js'
@@ -15,8 +15,11 @@ const port = process.env.PORT;
 app.use(express.json());
 app.use(cors());
 
-app.use("/api/user",userRouter);
+
 app.use("/api/chat",chatBot);
+app.use("/api/user",userRouter);
+app.use("/api/admin",adminRouter);
+app.use("/api/doctor",doctorRouter);
 
 app.listen(port, () => {
   connectDB();
